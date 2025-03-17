6adb1f12381cbdb7e5d803bcc6ddb9e41405a1a7
davidsalgadoo <davidguilherme12345@gmail.com>
2025-03-17 19:46:26 -0300

    atividadeflutter

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 113, 75, 250),
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 113, 75, 250),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(80),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 60,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 113, 75, 250),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(80),
+                        topRight: Radius.circular(80),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Get your Money \n Under Control',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 26, color: Colors.white),
+                softWrap: true,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Manage your expenses\nSeamlessly',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: const Color.fromARGB(255, 156, 156, 156),
+                ),
+              ),
+              SizedBox(height: 100),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 113, 75, 250),
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Sign Up with Email ID',
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 5),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      margin: EdgeInsets.only(right: 10),
+                      child: Image.network(
+                        'https://img.icons8.com/?size=100&id=17949&format=png&color=000000',
+                      ),
+                    ),
+                    Text(
+                      'Sign Up with Google',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 70),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account? ',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  Text(
+                    'Sign in',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
